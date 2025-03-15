2ccc7dc2f910d9721c5e9d76d7044d7df05f964f
veronica <vmuriga@andrew.cmu.edu>
2025-03-15 10:46:27 -0400

    updating throughput

---
+++ update_throughput.sh
@@ -0,0 +1,7 @@
+while true
+do
+git add .
+git commit -m "updating throughput"
+git push origin main 
+sleep 300
+done
--- veronica_throughput_info.py
+++ veronica_throughput_info.py
@@ -2,10 +2,10 @@
 import starlink_grpc, time, csv
 
 while True:
-    with open('/home/debroglie2/march7throughput.csv','a') as fd:
+    with open('/home/debroglie2/starlink-grpc-tools/march15throughput.csv','a') as fd:
         status = starlink_grpc.status_data()
         # print(status[0])
         newrow = [time.time_ns(), status[0]["uplink_throughput_bps"], status[0]["downlink_throughput_bps"]]
         writer = csv.writer(fd)
         writer.writerow(newrow)
-        time.sleep(0.1)
+        time.sleep(60)
